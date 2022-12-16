c32c9cd92997e3a0b21fe13a5065a8ad095f4b15
SanonZel <kanatovaha@gmail.com>
2022-12-17 01:53:17 +0600

    dz3

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Products from "./components/goods/Products"
+import Input from "./components/input/Input"
+import Menu from "./components/menu/Menu"
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Menu/>
+      <Input/>
+      <Products/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Comt.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Comt() {
+  return (
+    <div>Comt</div>
+  )
+}
+
+export default Comt
